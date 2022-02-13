18a03e77b80b35c221e88fe94aadbf0e9223ff65
Bastien2020 <bastien.mechard@gmail.com>
2022-02-13 18:09:14 +0100

    Changes to be committed: 	modified:   Publish_Luxmetre.py

--- Publish_Luxmetre.py
+++ Publish_Luxmetre.py
@@ -17,23 +17,23 @@ if __name__ == '__main__':
 	#file = open("data_luxmetre.csv", "w")
 	#file.write("timestamp" + "," + "Lux" + "\n")
 
-	while not rospy.is_shutdown():
-		# récupération de la mesure d'éclairement
-		measure = FL.data_collect(rge)
-		msg = String()
-		msg.data = measure
-		pub.publish(msg)
-		
-		#enregistrement de la mesure dans un fichier 
-		"""
-		now = datetime.now()
-		timestamp = datetime.timestamp(now)
+while not rospy.is_shutdown():
+	# récupération de la mesure d'éclairement
+	measure = FL.data_collect(rge)
+	msg = String()
+	msg.data = measure
+	pub.publish(msg)
 
-		try:
-			file.write(str(timestamp) + "," + measure + "\n")
-		except:
-			pass
-		"""
-		rate.sleep()
+	#enregistrement de la mesure dans un fichier
+	"""
+	now = datetime.now()
+	timestamp = datetime.timestamp(now)
 
-	rospy.loginfo("Node was stopped")
+	try:
+		file.write(str(timestamp) + "," + measure + "\n")
+	except:
+		pass
+	"""
+	rate.sleep()
+
+rospy.loginfo("Node was stopped")
